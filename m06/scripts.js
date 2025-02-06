88962a67ca90b1c03594860e802fd26dd16d3a85
enguyennn <161381297+enguyennn@users.noreply.github.com>
2025-02-06 15:09:16 -0800

    Update scripts.js
    got rid of extra code

--- m06/scripts.js
+++ m06/scripts.js
@@ -3,17 +3,11 @@ if (table == NaN || table == null){
   table = 0;
 }
 
-var operator = 'addition'; // Type of calculation
+var operator = 'multiplication'; // Type of calculation
 var i = 1;                 // Set counter to 1
 var msg = "<h2>Multiplication Table</h2>";              // Message
 
-if (operator === 'addition') {
-  // Do addition
-  while (i < 11) {
-    msg += i + ' + ' + table + ' = ' + (i + table) + '<br />';
-    i++;
-  }
-} else {
+if (operator == 'multiplication') {
   // Do multiplication
   while (i < 11) {
     msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
