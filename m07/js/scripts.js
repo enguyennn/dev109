9c852b8eb02f8cdef838fbcd6faa5149889ea900
enguyennn <161381297+enguyennn@users.noreply.github.com>
2025-02-21 23:54:57 -0800

    made minor updates to css and added third hotel to html and js

--- m07/js/scripts.js
+++ m07/js/scripts.js
@@ -9,10 +9,10 @@ function Hotel(name, rooms, booked) {
   }
   
   
-  // Create two hotel objects
+  // Create three hotel objects
   var quayHotel = new Hotel('Quay', 40, 25);
   var parkHotel = new Hotel('Park', 120, 77);
-  
+  var sunsetHotel = new Hotel('Sunset', 86, 10)
   
   // Update the HTML for the page
   var details1 = quayHotel.name + ' rooms: ';
@@ -24,8 +24,7 @@ function Hotel(name, rooms, booked) {
       details2 += parkHotel.checkAvailability();
   var elHotel2 = document.getElementById('hotel2');
   elHotel2.textContent = details2;
-  
-  /* 
-  NOTE: textContent does not work in IE8 or earlier
-  You can use innerHTML on lines 21 and 26, but note the security issues on p228-231
-  */
+
+  var details3 = sunsetHotel.name + ' rooms: ' + sunsetHotel.checkAvailability();
+  var elHotel3 = document.getElementById('hotel3');
+  elHotel3.textContent = details3;
