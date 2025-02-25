719ebd82ed5e283092e8e12ac8cddcd4a7137431
enguyennn <161381297+enguyennn@users.noreply.github.com>
2025-02-25 09:27:43 -0800

    added function to run during on-click

--- m08/js/scripts.js
+++ m08/js/scripts.js
@@ -1,14 +1,19 @@
-// Create a new element and store it in a variable.
-var newEl = document.createElement('li');
-
-// Create a text node and store it in a variable.
-var newText = document.createTextNode('quinoa');
-
-// Attach the new text node to the new element.
-newEl.appendChild(newText);
-
-// Find the position where the new element should be added.
-var position = document.getElementsByTagName('ul')[0];
-
-// Insert the new element into its position.
-position.appendChild(newEl);
+//Function to add user-inputted item to the list
+function addItem(item) {
+    // Create a new element and store it in a variable.
+    var newEl = document.createElement('li');
+    
+    
+    // Create a text node with the item name and store it in a variable.
+    var newText = document.createTextNode(item);
+    
+    // Attach the new text node to the new element.
+    newEl.appendChild(newText);
+  
+    // Find the position where the new element should be added.
+    var position = document.getElementsByTagName('ul')[0];
+  
+    // Insert the new element into its position.
+    position.appendChild(newEl);
+    
+  }
