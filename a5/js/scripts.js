542416eb01a846a9d4f3398d7893956a906ada09
enguyennn <161381297+enguyennn@users.noreply.github.com>
2025-03-13 21:01:46 -0700

    fixed the <br>

--- a5/js/scripts.js
+++ a5/js/scripts.js
@@ -1,13 +1,13 @@
 const images = [
-    {src: "images/thank-me-later.jpg", description: "Thank Me Later (2010) - Drake"<br>"Thank Me Later was Drake's debut studio album with 14 tracks.", 
+    {src: "images/thank-me-later.jpg", description: "Thank Me Later (2010) - Drake<br>Thank Me Later was Drake's debut studio album with 14 tracks.", 
         alt: "Album cover; black and white picture of drake looking upwards with red coloration"},
-    {src: "images/take-care.jpg", description: "Take Care (2011) - Drake"<br>"Take Care was Drake's second studion album with the deluxe version having 19 tracks.", 
+    {src: "images/take-care.jpg", description: "Take Care (2011) - Drake<br>Take Care was Drake's second studion album with the deluxe version having 19 tracks.", 
         alt: "Album cover; Drake looking at a golden chalice with a large golden candle adjacent"},
-    {src: "images/nothing-was-the-same.png", description: "Nothing Was The Same (2013) - Drake"<br>"Nothing Was The Same was Drake's third studio album with 13 tracks, and contains one of my personal favorites, 'Hold On, We're Going Home'.",
+    {src: "images/nothing-was-the-same.png", description: "Nothing Was The Same (2013) - Drake<br>Nothing Was The Same was Drake's third studio album with 13 tracks, and contains one of my personal favorites, 'Hold On, We're Going Home'.",
         alt: "Album cover; Baby with an afro and comb in hair looking right, background is a blue sky filled with clouds"},
-    {src: "images/views.jpg", description: "Views (2016) - Drake"<br>"Views was Drake's fourth studion album with 20 tracks, and contains some of his most popular hits.", 
+    {src: "images/views.jpg", description: "Views (2016) - Drake<br>Views was Drake's fourth studion album with 20 tracks, and contains some of his most popular hits.", 
         alt: ""},
-    {src: "images/scorpion.jpg", description: "Scorpion (2018) - Drake"<br>"Scorpion was Drake's fifth studio album with 25 tracks.", 
+    {src: "images/scorpion.jpg", description: "Scorpion (2018) - Drake<br>Scorpion was Drake's fifth studio album with 25 tracks.", 
         alt: "Album cover; black and white picture of Drake with his signature"}
 ];
 
