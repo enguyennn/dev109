54873992c9d02cad6c897253822dad0bf520bac3
enguyennn <161381297+enguyennn@users.noreply.github.com>
2025-03-13 20:30:10 -0700

    js debugging

--- a5/js/scripts.js
+++ a5/js/scripts.js
@@ -3,7 +3,7 @@ const images = [
         alt: "Album cover; black and white picture of drake looking upwards with red coloration"},
     {src: "images/take-care.jpg", description: "Take Care (2011) - Drake\nTake Care was Drake's second studion album with the deluxe version having 19 tracks.", 
         alt: "Album cover; Drake looking at a golden chalice with a large golden candle adjacent"},
-    {src: "images/nothing-was-the-same.png", description: "Nothing Was The Same (2013) - Drake\nNothing Was The Same was Drake's third studio album with 13 tracks, and contains one of my personal favorites, 'Hold On, We're Going Home'.,
+    {src: "images/nothing-was-the-same.png", description: "Nothing Was The Same (2013) - Drake\nNothing Was The Same was Drake's third studio album with 13 tracks, and contains one of my personal favorites, 'Hold On, We're Going Home'.",
         alt: "Album cover; Baby with an afro and comb in hair looking right, background is a blue sky filled with clouds"},
     {src: "images/views.jpg", description: "Views (2016) - Drake\nViews was Drake's fourth studion album with 20 tracks, and contains some of his most popular hits.", 
         alt: ""},
@@ -60,8 +60,8 @@ function timerAdjustment(){
   }
 }
 
-pButton.eventListener("click", previousSlide);
-nButton.eventListener("click", nextSlide);
+pButton.addEventListener("click", previousSlide);
+nButton.addEventListener("click", nextSlide);
 
 updateSlide(currentIndex);
 startTimer();
