66677d7c978aeb9dc3a99040360468bfbfefc664
enguyennn <161381297+enguyennn@users.noreply.github.com>
2025-03-15 12:25:14 -0700

    added code from js bin

---
+++ i-spy/js/scripts.js
@@ -0,0 +1,44 @@
+const total = 5;
+let found = 0;
+
+function foundItem(name){
+  let item = document.getElementById(name);
+  if (!item.classList.contains("f")){
+    item.classList.add("f");
+    item.style.textDecoration="line-through";
+    found++;
+    
+    if (found === total){
+      document.getElementById('again').style.display = block;
+    }
+  }
+}
+
+var bananaArea = document.getElementById("Banana")
+bananaArea.addEventListener("click", function() {
+  foundItem("Banana");
+}, false);
+
+var fanArea = document.getElementById("Fan");
+fanArea.addEventListener("click", function () {
+  foundItem("fan");
+}, false);
+
+var dartArea = document.getElementById("Dart");
+dartArea.addEventListener("click", function () {
+  foundItem("dart");
+}, false);
+
+var lionArea = document.getElementById("Lion");
+lionArea.addEventListener("click", function () {
+  foundItem("lion");
+}, false);
+
+var appleArea = document.getElementById("Apple");
+appleArea.addEventListener("click", function () {
+  foundItem("apple");
+}, false);
+
+function replay(){
+  location.reload();
+}
